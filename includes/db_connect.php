a67ec77f3982ae8a26f686efd17a1ecd684b8866
Kavya Balu <85859208+KavyaBalu@users.noreply.github.com>
2025-03-21 15:06:44 +0530

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "collaborative_work";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "collaborative_work";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 ?>
 <?php
 // function getDBConnection() {
@@ -28,4 +28,25 @@ if ($conn->connect_error) {
     // // Create a global connection object
     // $conn = getDBConnection();
     
+?>
+
+<?php
+$host = "workcommunity.mysql.database.azure.com";
+$username = "vidya";
+$password = "Kavyabalu@2000";
+$database = "{your_database}";
+$port = 3306;
+
+// Initialize connection
+$conn = mysqli_init();
+
+// Set SSL (if required, otherwise remove this part)
+mysqli_ssl_set($conn, NULL, NULL, "{path to CA cert}", NULL, NULL);
+
+// Establish a secure connection
+if (!mysqli_real_connect($conn, $host, $username, $password, $database, $port, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
 ?>
