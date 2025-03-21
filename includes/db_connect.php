0424486fc3ceac397b1e0d464628c0cc352f5cc9
Kavya Balu <85859208+KavyaBalu@users.noreply.github.com>
2025-03-21 15:29:51 +0530

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,52 +1,33 @@
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "collaborative_work";
-
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-?>
-<?php
 function getDBConnection() {
-        $host = "workcommunity.mysql.database.azure.com";
-        $username = "vidya";
-        $password = "Kavyabalu@2000";
-        $database = "workdata";
-        
-        $conn = mysqli_connect($host, $username, $password, $database);
-        
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        
-        return $conn;
+    $host = "workcommunity.mysql.database.azure.com";
+    $username = "vidya";
+    $password = "Kavyabalu@2000";
+    $database = "workdata";
+    $port = 3306;
+
+    // Enable MySQLi error reporting
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+    // Initialize connection
+    $conn = mysqli_init();
+
+    // Uncomment if SSL is required (use correct CA path)
+    // mysqli_ssl_set($conn, NULL, NULL, "/path/to/ca.pem", NULL, NULL);
+
+    // Establish connection
+    if (!mysqli_real_connect($conn, $host, $username, $password, $database, $port)) {
+        die("Connection failed: " . mysqli_connect_error());
     }
-    
-    // Create a global connection object
-    $conn = getDBConnection();
-    
-?>
 
-<?php
-// $host = "workcommunity.mysql.database.azure.com";
-// $username = "vidya";
-// $password = "Kavyabalu@2000";
-// $database = "workdata";
-// $port = 3306;
+    return $conn;
+}
 
-// // Initialize connection
-// $conn = mysqli_init();
+// Create a global connection object
+$conn = getDBConnection();
 
-// // Set SSL (if required, otherwise remove this part)
-// mysqli_ssl_set($conn, NULL, NULL, "{path to CA cert}", NULL, NULL);
-
-// // Establish a secure connection
-// if (!mysqli_real_connect($conn, $host, $username, $password, $database, $port, MYSQLI_CLIENT_SSL)) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-
-
+// Check connection
+if ($conn) {
+    echo "Connected successfully!";
+}
 ?>
