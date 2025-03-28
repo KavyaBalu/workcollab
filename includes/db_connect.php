3cabf0c19e03486f1922c8fd90bd1a420c2e4e04
Kavya Balu <85859208+KavyaBalu@users.noreply.github.com>
2025-03-28 14:21:18 +0530

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,8 +1,33 @@
 <?php
-$con = mysqli_connect("workcommunity.mysql.database.azure.com", "balu", "vidya@123", "collabdata", 3306);
+function getDBConnection() {
+    $host = "workcommunity.mysql.database.azure.com";
+    $username = "balu";
+    $password = "vidya@123";
+    $database = "collabdata";
+    $port = 3306;
 
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
+    // Enable MySQLi error reporting
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+    // Initialize connection
+    $conn = mysqli_init();
+
+    // Uncomment if SSL is required (use correct CA path)
+    // mysqli_ssl_set($conn, NULL, NULL, "/path/to/ca.pem", NULL, NULL);
+
+    // Establish connection
+    if (!mysqli_real_connect($conn, $host, $username, $password, $database, $port)) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    return $conn;
 }
 
+// Create a global connection object
+$conn = getDBConnection();
+
+// Check connection
+if ($conn) {
+    echo "Connected successfully!";
+}
 ?>
