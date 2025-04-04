250d32e05d38a12e119d4e0331efbfa3e740c8b4
Kavya Balu <85859208+KavyaBalu@users.noreply.github.com>
2025-04-04 14:57:27 +0530

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -32,15 +32,4 @@ if ($conn) {
 }
 ?>
 
-<?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// // $dbname = "collaborative_work";
-// $dbname="wildr";
 
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-?>
